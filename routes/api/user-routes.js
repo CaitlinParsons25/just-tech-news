fa08c5dc7c6972f16f51d5861c84fff064e2f0a0
CaitlinParsons25 <103784858+CaitlinParsons25@users.noreply.github.com>
2022-08-02 19:01:33 -0500

    troubleshooting

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+
 // GET /api/users
 router.get('/', (req, res) => {
     User.findAll({
@@ -35,7 +36,7 @@ router.get('/:id', (req, res) => {
 
 // POST /api/users
 router.post('/', (req, res) => {
-    User.create({
+   User.create({
         username: req.body.username,
         email: req.body.email,
         password: req.body.password
